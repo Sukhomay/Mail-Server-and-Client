d96e2960e73f5e4cf5fd153353478b15dc7fbe69
Abir <royabirlebachun0@gmail.com>
2024-01-27 21:43:32 +0530

    client updated

--- mailclient.c
+++ mailclient.c
@@ -15,9 +15,9 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    const char *server_IP = argv[1];
+    const char *serverIP = argv[1];
     int smtpPort = atoi(argv[2]);
-    int pop3_port = atoi(argv[3]);
+    int pop3Port = atoi(argv[3]);
 
     char username[100];
     printf("Enter username: ");
@@ -39,7 +39,7 @@ int main(int argc, char *argv[]) {
                 break;
             case 2:
                 // Send mail functionality
-                send_mail(smtp_port, server_IP, username);
+                send_mail(smtpPort, serverIP, username);
                 break;
             case 3:
                 printf("Quitting the program.\n");
@@ -52,7 +52,8 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void send_mail(int smtp_port, const char *server_IP, const char *username) {
+void send_mail(int smtp_port, const char *server_IP, const char *username)
+{
     int smtp_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (smtp_socket == -1) {
         perror("Error creating SMTP socket");
@@ -64,21 +65,23 @@ void send_mail(int smtp_port, const char *server_IP, const char *username) {
     server_address.sin_port = htons(smtp_port);
     server_address.sin_addr.s_addr = inet_addr(server_IP);
 
-    if (connect(smtp_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
+    if (connect(smtp_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1)
+    {
         perror("Error connecting to SMTP server");
         exit(EXIT_FAILURE);
     }
 
     char buffer[MAX_BUFFER_SIZE];
-    ssize_t bytes_received;
+    size_t bytesReceived;
 
     // Receive initial greeting from server
-    bytes_received = recv(smtp_socket, buffer, sizeof(buffer), 0);
-    if (bytes_received == -1) {
+    bytesReceived = recv(smtp_socket, buffer, sizeof(buffer), 0);
+    if (bytesReceived == -1)
+    {
         perror("Error receiving from SMTP server");
         exit(EXIT_FAILURE);
     }
-    buffer[bytes_received] = '\0';
+    buffer[bytesReceived] = '\0';
     printf("%s", buffer);  // Print server's initial response
 
     // Sending HELO command
@@ -86,37 +89,80 @@ void send_mail(int smtp_port, const char *server_IP, const char *username) {
     send(smtp_socket, buffer, strlen(buffer), 0);
 
     // Receiving response for HELO command
-    bytes_received = recv(smtp_socket, buffer, sizeof(buffer), 0);
-    if (bytes_received == -1) {
+    bytesReceived = recv(smtp_socket, buffer, sizeof(buffer), 0);
+    if (bytesReceived == -1) 
+    {
         perror("Error receiving from SMTP server");
         exit(EXIT_FAILURE);
     }
-    buffer[bytes_received] = '\0';
+    buffer[bytesReceived] = '\0';
     printf("%s", buffer);  // Print server's response to HELO command
 
     // Get mail from user
+    char recipient[100];
+    char subject[100];
     printf("Enter mail to be sent in the specified format:\n");
     printf("From: <%s>\n", username);
     printf("To: ");
-    scanf("%s", buffer); // Assume only one recipient for simplicity
-    printf("Subject: ");
-    scanf(" %[^\n]s", buffer); // Read subject with spaces
-    printf("Message body (End with a single dot on a new line):\n");
+    scanf("%s", recipient); // Assume only one recipient for simplicity
 
-    // Construct and send mail data
-    snprintf(buffer, sizeof(buffer), "From: <%s>\r\nTo: <%s>\r\nSubject: %s\r\n", username, buffer, buffer);
-    send(smtp_socket, "DATA\r\n", 6, 0);
+
+    snprintf(buffer, sizeof(buffer), "MAIL FROM: %s\r\n", username);
     send(smtp_socket, buffer, strlen(buffer), 0);
-    send(smtp_socket, "\r\n.\r\n", 5, 0);
 
-    // Receive response for mail data
-    bytes_received = recv(smtp_socket, buffer, sizeof(buffer), 0);
-    if (bytes_received == -1) {
+    // Receiving response for MAIL command
+    bytesReceived = recv(smtp_socket, buffer, sizeof(buffer), 0);
+    if (bytesReceived == -1) 
+    {
         perror("Error receiving from SMTP server");
         exit(EXIT_FAILURE);
     }
-    buffer[bytes_received] = '\0';
-    printf("%s", buffer);  // Print server's response to mail data
+    buffer[bytesReceived] = '\0';
+    printf("%s", buffer);  // Print server's response to MAIL command
+    
+    snprintf(buffer, sizeof(buffer), "RCPT TO: %s\r\n", recipient);
+    send(smtp_socket, buffer, strlen(buffer), 0);
+
+    // Receiving response for RCPT command
+    bytesReceived = recv(smtp_socket, buffer, sizeof(buffer), 0);
+    if (bytesReceived == -1) 
+    {
+        perror("Error receiving from SMTP server");
+        exit(EXIT_FAILURE);
+    }
+    buffer[bytesReceived] = '\0';
+    printf("%s", buffer);  // Print server's response to RCPT command
+
+
+    printf("Subject: ");
+    scanf(" %[^\n]s", subject); // Read subject with spaces
+
+    // Construct and send mail data
+    snprintf(buffer, sizeof(buffer), "From: <%s>\r\nTo: <%s>\r\nSubject: %s\r\n", username, recipient, subject);
+    send(smtp_socket, "DATA\r\n", 6, 0);
+    send(smtp_socket, buffer, strlen(buffer), 0);
+
+
+    printf("Message body (End with a single dot on a new line):\n");
+    char dataLine[100];
+    while(1)
+    {
+        scanf(" %[^\n]s", dataLine);
+        snprintf(buffer, sizeof(buffer), "%s\r\n", dataLine);
+        send(smtp_socket, buffer, strlen(buffer), 0);
+
+        if(strcmp(dataLine, ".") == 0)
+            break;
+    }
+
+    // Receive response for command DATA
+    bytesReceived = recv(smtp_socket, buffer, sizeof(buffer), 0);
+    if (bytesReceived == -1) {
+        perror("Error receiving from SMTP server");
+        exit(EXIT_FAILURE);
+    }
+    buffer[bytesReceived] = '\0';
+    printf("%s", buffer);  // Print server's response to command DATA
 
     // Close connection with SMTP server
     send(smtp_socket, "QUIT\r\n", 6, 0);
